5ce989c7c4ce4b3290cfa830264fbecf7fe88ae6
Juan Carlos Quiñonez Madrid <149096035+CarlosMadrid11@users.noreply.github.com>
2024-03-07 16:22:58 +0000

    cambios pendientes

--- miPrincipal/AppLadrillos.java
+++ miPrincipal/AppLadrillos.java
@@ -1,10 +1,26 @@
 package miPrincipal;
 public class AppLadrillos{
 
+
+
     public static void menu(){
+        System.out.println("Version iterativa");
+        dibujar_pared_ite(10);
+        System.out.println("Version Recursiva");
+        dibujar_pared_rec(10);
 
+        }
+        public static void dibujar_pared_ite(int N){
+            for(int i=0; 1<N ; i++){
+                System.out.println("xxxxxxxxxxxxxxxxx");
 
+        }
 
+        public static void dibujar_pared_rec(int N){
+            for(int i=0; 1<N ; i++){
+                dibujar_pared_rec(N-1);
+                System.out.println("xxxxxxxxxxxxxxxxx");
+        }
 
 
 
